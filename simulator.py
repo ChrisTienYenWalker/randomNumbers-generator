4b5a6e2fd4ed156e1884545bae2eccf684703a9c
Master4020 <47930042+Master4020@users.noreply.github.com>
2019-04-03 18:13:27 -0400

    finished the project to the next

--- simulator.py
+++ simulator.py
@@ -1,10 +1,13 @@
 from random import *
+count = 0
 numbers = [1, 2, 3, 4, 5, 6]
 pick = sample(numbers, 1)
 print("your dice roll number is")
 print(pick)
 print("and your lucky numbers are")
-print(randint(1, 100))
+while count < 6:
+    print(randint(1, 100))
+    count += 1
 
 
 
